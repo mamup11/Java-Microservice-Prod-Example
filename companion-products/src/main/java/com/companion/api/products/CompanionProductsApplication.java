bfb3b96a304f490255834a31b1287ea2d7f6a353
mateo <mateo.murillo@somoglobal.com>
2020-08-19 19:18:56 -0500

    Adding new microservice proyect companion-products

---
+++ companion-products/src/main/java/com/companion/api/products/CompanionProductsApplication.java
@@ -0,0 +1,13 @@
+package com.companion.api.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanionProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanionProductsApplication.class, args);
+    }
+
+}
