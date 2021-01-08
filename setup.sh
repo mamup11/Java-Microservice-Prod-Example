72bb763641b525ad074b155dce3e73cce33d0740
Mateo <mamup111@hotmail.com>
2021-01-07 21:18:05 -0500

    Solving issue with commons library and missing folder

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
 
+# Compile and add commons framework to local maven repository
+cd backend-commons
+mvn clean install -U
+
 # Compile Auth-Fast and build docker image
-cd auth-fast/
+cd ../auth-fast/
 mvn clean package
 docker build --no-cache --tag auth-fast:latest .
 
